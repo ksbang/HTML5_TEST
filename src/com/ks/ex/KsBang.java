d710de26e098716c99b6d0308edd7290f493b019
ksbang72 <sky@DESKTOP-S517OQJ>
2017-11-23 14:27:33 +0900

    [Test-001]테스트

---
+++ src/com/ks/ex/KsBang.java
@@ -0,0 +1,17 @@
+package com.ks.ex;
+
+public class KsBang {
+	
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		int c = a + b;
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
